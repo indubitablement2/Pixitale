04307db435d2e385ce45f5aa5e36d8a41e276da2
Jean-Michel Nadeau <jeanmichel88@hotmail.com>
2024-04-07 03:31:33 -0400

    Player animation tree

--- client/core/game/character/player/player.gd
+++ client/core/game/character/player/player.gd
@@ -5,13 +5,14 @@ static var local_player : Player
 ## {peer_id(int) : Player}
 static var _players := {}
 
-@export var body : Sprite2D
+@export var root_body : Node2D
 @export var arm_back : Sprite2D
 @export var hand_back : Node2D
 @export var arm_front : Sprite2D
-@export var hand_front : Node2D
 
 @export var animation_player : AnimationPlayer
+@export var animation_tree : AnimationTree
+
 @export var grid_body : GridBody
 
 var movement_speed := 460.0
@@ -21,9 +22,9 @@ var ground_friction := 0.04
 var gravity := 400.0
 
 var queue_jump := 0.0
+var jump_cooldown := 0.0
 ## How long since we were last on floor.
-var last_on_floor := 0.0
-var floor_jump_cooldown := 0.15
+var _last_on_floor := 0.0
 
 var inventory : Array[ItemInstance] = []
 
@@ -37,45 +38,31 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	_players.erase(get_multiplayer_authority())
 
-var t := 0.0
 func _process(delta: float) -> void:
+	arm_idle()
+	
 	if is_multiplayer_authority():
 		_movement(delta)
-	
-	#var wish_rot := 0.0
-	#if body.animation == &"Run":
-		#t += delta
-		#arm_back.rotation = sin(t * movement_speed * 0.02) * 0.6 - 0.2
-		#arm_front.rotation = -sin(t * movement_speed * 0.02) * 0.6
-	#if body.animation == &"Air":
-		#t += delta
-		#arm_back.rotation = -0.8
-		#arm_front.rotation = 0.6
-	
-	
-	$Label.set_text(str(
-		grid_body.is_on_floor(), "\n",
-		grid_body.is_on_ceiling(), "\n",
-		grid_body.is_on_left_wall(), "\n",
-		grid_body.is_on_right_wall()))
-	$Velocity.text = str(grid_body.velocity.round())
 
 func _movement(delta: float) -> void:
 	if grid_body.is_on_floor():
-		last_on_floor = 0.0
+		_last_on_floor = 0.0
 	else:
-		last_on_floor += delta
+		_last_on_floor += delta
 	
 	if Input.is_action_just_pressed(&"up"):
 		queue_jump = 0.1
 	
 	# Floor jump.
-	floor_jump_cooldown -= delta
-	if queue_jump > 0.0 && last_on_floor < 0.1 && floor_jump_cooldown < 0.0:
-		floor_jump_cooldown = 0.1
+	jump_cooldown -= delta
+	queue_jump -= delta
+	if queue_jump > 0.0 && _last_on_floor < 0.05 && jump_cooldown < 0.0:
+		jump_cooldown = 0.05
 		grid_body.velocity.y -= 200.0
 		queue_jump = 0.0
-	queue_jump -= delta
+		_set_jump_animation(true)
+	else:
+		_set_jump_animation(false)
 	
 	var wish_dir := (Input.get_action_strength(&"right") - Input.get_action_strength(&"left"))
 	
@@ -88,20 +75,8 @@ func _movement(delta: float) -> void:
 				movement_speed * delta)
 		else:
 			grid_body.velocity.x += wish_dir * movement_speed * delta
-		
-		#if absf(grid_body.velocity.x) > 50.0 && signf(grid_body.velocity.x) == signf(wish_dir):
-			#body.animation = &"Run"
-		#else:
-			#body.animation = &"Idle"
 	else:
 		grid_body.velocity.x += wish_dir * movement_speed * delta * air_control
-		#body.animation = &"Air"
-	
-	# Look toward wish movement dir.
-	if wish_dir > 0.1:
-		body.scale.x = 1.0
-	elif wish_dir < -0.1:
-		body.scale.x = -1.0
 	
 	grid_body.velocity.y += gravity * delta
 	
@@ -115,13 +90,34 @@ func _movement(delta: float) -> void:
 			delta)
 	else:
 		grid_body.velocity.x *= pow(air_friction, delta)
+	
+	# Look toward wish movement dir.
+	if wish_dir > 0.1:
+		root_body.scale.x = 1.0
+	elif wish_dir < -0.1:
+		root_body.scale.x = -1.0
+	
+	_set_on_floor_animation(grid_body.is_on_floor())
+	_set_run_animation(!is_zero_approx(wish_dir) && !is_zero_approx(grid_body.velocity.x) && signf(grid_body.velocity.x) == signf(wish_dir))
+
+func _set_jump_animation(value: bool) -> void:
+	animation_tree.set(&"parameters/conditions/jump", value)
+
+func _set_run_animation(value: bool) -> void:
+	animation_tree.set(&"parameters/conditions/run", value)
+	animation_tree.set(&"parameters/conditions/not_run", !value)
+
+func _set_on_floor_animation(value: bool) -> void:
+	animation_tree.set(&"parameters/conditions/on_floor", value)
+	animation_tree.set(&"parameters/conditions/not_on_floor", !value)
+
+func arm_aim_mouse() -> void:
+	arm_back.look_at(Global.mouse_position)
+
+func arm_idle() -> void:
+	arm_back.rotation = -arm_front.rotation
 
 ## Return the peer's Player or null if not found.
 static func _get_peer_player(peer_id: int) -> Player:
 	return _players.get(peer_id)
-
-#@rpc("authority", "call_local")
-#func jump() -> void:
-	#pass
-
 
